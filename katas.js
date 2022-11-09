60b41e45d72a17b7fbfff7a1c2c7868230e4a2dc
Hadesal <hadealhlo1977@gmail.com>
2022-11-09 15:09:26 +0100

    many changes

--- katas.js
+++ katas.js
@@ -1,2 +1,26 @@
 // function removeExclamationMarks(s) { s.replace(/!/g,"");}
-// console.log(removeExclamationMarks("hello world !!"))
+// console.log(("hello world !!"))
+
+
+// let weight = 87;
+// let height = 1.80;
+// bmi = (weight,height) =>{
+//     let bm = (weight / Math.pow(height,2));
+//     console.log(bm)
+
+//     if(bm <18.5){
+//         return `Underweight`
+
+//     }else if(bm > 18.5 && bm < 24){
+//             return `Normal`;
+
+//     } else if(bm > 24 && bm < 30.0){
+//         return `Overweight`
+
+//     }else if(bm >30){
+//         return `Obese`
+
+//     }
+// }
+
+//getCount = str => str.match(/[aeiou]/gi).length
